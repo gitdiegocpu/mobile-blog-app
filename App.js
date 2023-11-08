e8ff2f7b996eacdfe30adf49ad91f8aa6c4aeff9
Diego F Garcia <gadifer54@gamil>
2023-11-08 13:34:37 -0500

    pictures

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
+
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, ScrollView, SafeAreaview } from 'react-native';
+import PostCard from "./components/PostCard";
 
 export default function App() {
+  const [blogPosts, setBlogPost] = useState([])
+    // useEffect is being called on different lifecycle of the component
+    //1. When components Mounts
+    //2. When components unMounts
+
+    //how to get a component to re-render
+    //1. Change te state variable
+    //2. Change props
+
+    const handleFetchData = () => {
+      console.log("fetchin data");
+
+
+      fetch(" http://192.168.10.185:8080", {
+        method: "GET",
+        headers: {"Content-type": "application/json"},
+      })
+      .then((res) => res.json())
+      .then((data) => setBlogPost(data))
+      .catch((err) => console.log(err));   
+    };     
+  }
+    useEffect(() => {
+        fetch('http://192.168.10.185:8080')
+           .then(res => res.json())
+           .then(data => setBlogPost(data))                  //missing
+           .catch(err => console.log(err));
+ 
+    }, []);
+  
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView >      
       <StatusBar style="auto" />
+      <view>
+        <ScrollView>
+             {blogPosts.map((singlePost, index) => {
+              
+             return <PostCard singlePost={singlePost} index={index} key={singlePost._id} />
+             })}
+      </ScrollView>
     </View>
+  </SafeAreaView>
+
   );
+            }
+    
+
+
+               
+                   
+          <Text>{singlePost.title}</Text>
+          </View>
+        )
+      })}
+     </ScrollView>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ assets/components/PostCard.js
@@ -0,0 +1,60 @@
+import { View,} from 
+
+
+export default function PostCard({ singlePost, index }) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      // display: 'flex',
+      // height: '100%',
+      // width: '100%',
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    cardContainer: {
+      marginVertical: 20,
+    },
+    avatarImage: {
+      borderRadius: 100,
+      marginRight: 10, 
+    }
+  
+    flex: 1,
+    aspectRadio: 1,
+    image: '100%',
+    width: '100%',
+  }, 
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
